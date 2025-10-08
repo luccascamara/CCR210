90ebdb830f4ecca47c541b87355fce6c476b5091
Cauê <141970034+Caue804@users.noreply.github.com>
2025-10-08 18:52:03 -0300

    wavefront

---
+++ workspace/wavefront/mapa.py
@@ -0,0 +1,13 @@
+# pip install pathfinding
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+from matplotlib import pyplot as plt
+
+pgmf = open('map.pgm', 'rb')
+matrix = plt.imread(pgmf)
+print (matrix)
+
+matrix = 1.0 * (matrix > 250)
+plt.imshow(matrix, interpolation='nearest', cmap='gray')
+plt.show()

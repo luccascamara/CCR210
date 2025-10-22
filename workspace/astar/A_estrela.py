a313c5d2711361228a00eea4df89c49fbc58b2fa
Cauê <141970034+Caue804@users.noreply.github.com>
2025-10-22 01:19:03 -0300

    a estrela

---
+++ workspace/astar/A_estrela.py
@@ -0,0 +1,32 @@
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+from matplotlib import pyplot as plt
+
+pgmf = open('map.pgm', 'rb')
+matrix = plt.imread(pgmf)
+print (matrix)
+
+matrix = 1.0 * (matrix > 250)
+plt.imshow(matrix, interpolation='nearest', cmap='gray')
+plt.show()
+
+x_inicio = 120
+y_inicio = 30
+
+x_final = 260
+y_final = 200
+
+grid = Grid(matrix=matrix)
+start = grid.node(x_inicio, y_inicio)
+end   = grid.node(x_final , y_final)
+
+finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+path, runs = finder.find_path(start, end, grid)
+print('operations:', runs, 'path length:', len(path))
+
+plt.imshow(matrix, interpolation='nearest', cmap='gray')
+for cell in path:
+    plt.scatter(x=cell.x, y=cell.y, c='r', s=5)
+
+plt.show()
